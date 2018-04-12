624ac5fcfdf156c3fdb0ba270d442e5cfb208e5b
Claudio <claudiohw@gmail.com>
2018-04-12 09:48:55 -0300

    Switching tabs to spaces

--- rollup.config.js
+++ rollup.config.js
@@ -10,52 +10,52 @@ import { sass } from 'svelte-preprocess-sass';
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-	input: 'src/main.js',	
-	output: {
-		sourcemap: true,	
-		format: 'iife',
-		name: 'app',
-		file: 'public/bundle.js'
-	},
-	plugins: [
-		serve({
-			contentBase: ['public'],
-			historyApiFallback: true,
-			port: 5100,
-			verbose: true,
-		}),
-		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
+  input: 'src/main.js',	
+  output: {
+    sourcemap: true,	
+    format: 'iife',
+    name: 'app',
+    file: 'public/bundle.js'
+  },
+  plugins: [
+    serve({
+      contentBase: ['public'],
+      historyApiFallback: true,
+      port: 5100,
+      verbose: true,
+    }),
+    svelte({
+      // enable run-time checks when not in production
+      dev: !production,
 
-			// we'll extract any component CSS out into
-			// a separate file — better for performance
-			css: css => {
-				css.write('public/bundle.css');
-			},
+      // we'll extract any component CSS out into
+      // a separate file — better for performance
+      css: css => {
+        css.write('public/bundle.css');
+      },
 
-			preprocess: {
+      preprocess: {
         style: sass(),
       },
 
-			// enable https://svelte.technology/guide#state-management
-			store: true,
+      // enable https://svelte.technology/guide#state-management
+      store: true,
 
-			// this results in smaller CSS files
-			cascade: false,
-		}),
+      // this results in smaller CSS files
+      cascade: false,
+    }),
 
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration —
-		// consult the documentation for details:
-		// https://github.com/rollup/rollup-plugin-commonjs
-		resolve(),
-		commonjs(),
+    // If you have external dependencies installed from
+    // npm, you'll most likely need these plugins. In
+    // some cases you'll need additional configuration —
+    // consult the documentation for details:
+    // https://github.com/rollup/rollup-plugin-commonjs
+    resolve(),
+    commonjs(),
 
-		// If we're building for production (npm run build
-		// instead of npm run dev), transpile and minify
-		production && buble({ exclude: 'node_modules/**' }),
-		production && uglify()
-	]
+    // If we're building for production (npm run build
+    // instead of npm run dev), transpile and minify
+    production && buble({ exclude: 'node_modules/**' }),
+    production && uglify()
+  ]
 };
